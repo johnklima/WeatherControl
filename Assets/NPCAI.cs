329a05983d8c2286260473c44e6a95662eaed587
johnklima <klima.de.sa@gmail.com>
2020-01-14 14:26:15 +0000

    more npc cone of visiblity

--- Assets/NPCAI.cs
+++ Assets/NPCAI.cs
@@ -11,12 +11,13 @@ public class NPCAI : MonoBehaviour
     public bool followPlayer = false;
 
     public Transform[] patrolPoints = new Transform[10];
+    public int curIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(player.position);
+        agent.SetDestination(patrolPoints[curIndex].position);
 
 
     }
@@ -24,23 +25,36 @@ public class NPCAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (agent.remainingDistance > 3.0f)
+        if (agent.remainingDistance < 10.0f && !followPlayer)
         {
-            agent.SetDestination(player.position);
-        }            
-        else if(cooldown < 0)
-        {
-            agent.isStopped = true;
-            cooldown = Time.time;
-        }
-        //reset destination and timer
-        if(cooldown > 0 && Time.time - cooldown > 3.0f)
-        {
-            cooldown = -1;
-            agent.isStopped = false;
-            agent.SetDestination(player.position);
+            curIndex++;
+            if (curIndex >= patrolPoints.Length)
+            {
+                curIndex = 0;
+            }
+
+            agent.SetDestination(patrolPoints[curIndex].position);
         }
 
+        if (followPlayer)
+        {
+            if (agent.remainingDistance > 3.0f)
+            {
+                agent.SetDestination(player.position);
+            }
+            else if (cooldown < 0)
+            {
+                agent.isStopped = true;
+                cooldown = Time.time;
+            }
+            //reset destination and timer
+            if (cooldown > 0 && Time.time - cooldown > 3.0f)
+            {
+                cooldown = -1;
+                agent.isStopped = false;
+                agent.SetDestination(player.position);
+            }
+        }
 
     }
 }
--- Assets/SeePlayer.cs
+++ Assets/SeePlayer.cs
@@ -24,11 +24,38 @@ public class SeePlayer : MonoBehaviour
         //if the cone collides with player, chase the player
         if (other.tag == "Player")
         {
-            Debug.Log(transform.parent.name + " see's player");
-            
-            //follow player
-            npcAIscript = transform.parent.GetComponent<NPCAI>();
-            npcAIscript.followPlayer = true;
+            Debug.Log(transform.parent.name + " might see player");
+
+
+            // Bit shift the index of the layer (8) to get a bit mask
+            int layerMask = 1 << 8;
+
+            // This would cast rays only against colliders in layer 8.
+            // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+            //layerMask = ~layerMask;
+
+            RaycastHit hit;
+            float distance = Vector3.Distance(transform.position, other.transform.position) - 1.0f;
+            bool didhit = false;
+
+            // Does the ray intersect any objects excluding the player layer
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, distance, layerMask))
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Did Hit");
+                didhit = true;
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+                Debug.Log("Did not Hit");
+            }
+
+            if (!didhit)
+            { //follow player
+                npcAIscript = transform.parent.GetComponent<NPCAI>();
+                npcAIscript.followPlayer = true;
+            }
 
 
         }
