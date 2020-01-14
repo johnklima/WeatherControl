c3e8e3f697a8b9462df2dd20e6ed16ba953a5c82
johnklima <klima.de.sa@gmail.com>
2020-01-14 13:11:42 +0000

    First NPC

---
+++ Assets/NPCAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCAI : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public Transform player;
+    public float cooldown = -1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(player.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent.remainingDistance > 3.0f)
+        {
+            agent.SetDestination(player.position);
+        }            
+        else if(cooldown < 0)
+        {
+            agent.isStopped = true;
+            cooldown = Time.time;
+        }
+        //reset destination and timer
+        if(cooldown > 0 && Time.time - cooldown > 3.0f)
+        {
+            cooldown = -1;
+            agent.isStopped = false;
+            agent.SetDestination(player.position);
+        }
+
+
+    }
+}
