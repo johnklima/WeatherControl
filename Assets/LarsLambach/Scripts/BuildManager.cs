ca0c693df4e4cf63ede64a88ad8ff29a21124ecf
larsmartin310 <larsmartinsisen@hotmail.no>
2020-01-14 14:41:17 +0100

    PlantCrop

---
+++ Assets/LarsLambach/Scripts/BuildManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject standardCropPrefab;
+
+    void Start()
+    {
+        cropToBuild = standardCropPrefab;
+    }
+
+    private GameObject cropToBuild;
+
+    public GameObject GetCropToBuild()
+    {
+        return cropToBuild;
+    }
+    
+}
