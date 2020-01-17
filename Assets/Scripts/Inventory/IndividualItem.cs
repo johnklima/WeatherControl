bf4cb3ad175c0cccc1af3fb6e6d48bbbdc1cc340
JohnH <51337462+JohnHauge@users.noreply.github.com>
2020-01-17 20:36:32 +0100

    Interface
    NumberSelect seed type, bug on the "0" button, not sure what is happening right now

---
+++ Assets/Scripts/Inventory/IndividualItem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IndividualItem : MonoBehaviour
+{
+    public int SelectionKey;
+    private KeyCode theKey;
+    private bool isBold;
+
+    private ItemSelected daddy;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Make custom keycode using attached int value & string
+        string keyString = "Alpha" + SelectionKey.ToString();
+        theKey = (KeyCode)System.Enum.Parse(typeof(KeyCode), keyString);
+        daddy = FindObjectOfType<ItemSelected>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this key is selected
+        if (Input.GetKeyDown(theKey))
+        {
+            GetComponent<Text>().fontStyle = FontStyle.Bold;
+            isBold = true;
+            daddy.selectedItem = SelectionKey;
+        }
+        //this key isn't seleceted
+        else if (Input.anyKeyDown == true && isBold == true)
+        {
+            GetComponent<Text>().fontStyle = FontStyle.Normal;
+            isBold = false;
+        }
+    }
+}
