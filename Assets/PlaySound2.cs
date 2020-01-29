3c40047fbd8b4fc3e571184113e4f4471d4feaad
Morten Aleksander U. Iversen <43225084+MidnightK@users.noreply.github.com>
2020-01-29 10:29:49 +0100

    more soundfx work

--- Assets/PlaySound2.cs
+++ Assets/PlaySound2.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlaySound : MonoBehaviour
+public class PlaySound2 : MonoBehaviour
 {
-
     public AudioClip AmbientSound;
     public float Volume;
     public AudioSource audio;
-    public float startCount = 15f;
-    public float currentCount = 15f;
+    public float startCount = 10f;
+    public float currentCount = 10f;
 
     // Update is called once per frame
     void Update()
     {
         currentCount -= Time.deltaTime;
 
-        if(currentCount <= 0)
+        if (currentCount <= 0)
         {
             audio.PlayOneShot(AmbientSound, Volume);
             currentCount = startCount;
