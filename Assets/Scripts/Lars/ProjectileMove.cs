fc21d9489358137e5606a5abf3d0dc0d2688e928
larsmartin310 <larsmartinsisen@hotmail.no>
2020-01-29 21:45:57 +0100

    check

---
+++ Assets/Scripts/Lars/ProjectileMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMove : MonoBehaviour
+{
+
+    public float speed; //how fast it travels
+    public GameObject impactPrefab; //impact vfx prefab.
+    public List<GameObject> trails;
+
+    private Rigidbody rb;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); //cache to rb to get to component rigidbody
+    }
+
+   
+    void FixedUpdate() //dont want to skip frames
+    {   //if speed is different than 0, if rb is different than null / if it has been instantiated
+        if(speed != 0 && rb != null)
+        {
+            rb.position += transform.forward * (speed * Time.deltaTime); //making it move
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        speed = 0;
+
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+
+        if(impactPrefab != null)
+        {
+            var impactVFX = Instantiate(impactPrefab, pos, rot) as GameObject;
+            Destroy(impactVFX, 8);
+        }
+
+        if (trails.Count > 0)
+        {
+            for(int i = 0; i< trails.Count; i++)
+            {
+                trails[i].transform.parent = null;
+                var ps = trails[i].GetComponent<ParticleSystem>();
+                if (ps != null)
+                {
+                    ps.Stop();
+                    Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+                }
+            }
+        }
+
+        Destroy(gameObject);
+
+       
+    }
+}
---
+++ Assets/Scripts/Lars/SpawnVFX.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnVFX : MonoBehaviour
+{
+    public GameObject vfx; //our vfx prefab to spawn
+    public Transform startPoint; //where it will spawn, a.      //you create a empty gameobject, startPoint, make it a child of empty gameobject, endPoint.
+    public Transform endPoint; //where it will travel to, b     //Place this script onto parent gameobject, place prefabs in slots, make this parent gameobject into prefab.
+  
+    void Start()
+    {
+        var startPos = startPoint.position; //the position of the start point
+        GameObject objVFX = Instantiate(vfx, startPos, Quaternion.identity) as GameObject; //instantiating the vfx prefab, at our startPoint
+
+        var endPos = endPoint.position; //the location of the endPoint
+
+        RotateTo(objVFX, endPos); //rotates it to move towards the endPos position
+    }
+
+    void RotateTo(GameObject obj, Vector3 destination) //declaring a function
+    {
+        var direction = destination - obj.transform.position;
+        var rotation = Quaternion.LookRotation(direction);
+        obj.transform.localRotation = Quaternion.Lerp(obj.transform.rotation, rotation, 1);
+    }
+}
